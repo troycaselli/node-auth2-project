c50e7b512ed84c688c18a468265f6833c74fb38c
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-12-08 10:05:38 -0800

    created findBy model

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -4,40 +4,13 @@ function find() {
   return db('users')
     .select('user_id', 'username', 'role_name')
     .join('roles', 'users.role_id', '=', 'roles.role_id')
-
-  /**
-    You will need to join two tables.
-    Resolves to an ARRAY with all users.
-
-    [
-      {
-        "user_id": 1,
-        "username": "bob",
-        "role_name": "admin"
-      },
-      {
-        "user_id": 2,
-        "username": "sue",
-        "role_name": "instructor"
-      }
-    ]
-   */
 }
 
 function findBy(filter) {
-  /**
-    You will need to join two tables.
-    Resolves to an ARRAY with all users that match the filter condition.
-
-    [
-      {
-        "user_id": 1,
-        "username": "bob",
-        "password": "$2a$10$dFwWjD8hi8K2I9/Y65MWi.WU0qn9eAVaiBoRSShTvuJVGw8XpsCiq",
-        "role_name": "admin",
-      }
-    ]
-   */
+  return db('users')
+    .select('user_id', 'username', 'password', 'role_name')
+    .join('roles', 'users.role_id', '=', 'roles.role_id')
+    .where(filter)
 }
 
 function findById(user_id) {
